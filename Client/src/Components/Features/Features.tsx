bbe0e65d3f5d251d6f1b70176131ebc46b0067e8
Anas <110332632+anasAS02@users.noreply.github.com>
2023-11-06 14:43:06 +0200

    .

--- Client/src/Components/Features/Features.tsx
+++ Client/src/Components/Features/Features.tsx
@@ -3,16 +3,16 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 export const Features = () => {
   return (
-    <div className='flex items-center justify-center gap-10'>
-        <p className='text-lg font-bold flex items-center gap-5 border-r-green-400 border-r-2'>
+    <div className='flex items-center justify-center gap-10 flex-wrap'>
+        <p className='text-lg font-bold flex items-center gap-5 max-md:gap-2 md:border-r-green-400 md:border-r-2'>
             Fast shipping
             <FontAwesomeIcon icon={faTruckFast} className='text-green-400 w-[50px] h-[50px] mr-5' />
         </p>
-        <p className='text-lg font-bold flex items-center gap-5 border-r-green-400 border-r-2'>
+        <p className='text-lg font-bold flex items-center gap-5 max-md:gap-2 md:border-r-green-400 md:border-r-2'>
             Competitive prices
             <FontAwesomeIcon icon={faCreditCard} className='text-green-400 w-[50px] h-[50px] mr-5' />
         </p>
-        <p className='text-lg font-bold flex items-center gap-5'>
+        <p className='text-lg font-bold flex items-center gap-5  max-md:gap-2'>
             Diverse products
             <FontAwesomeIcon icon={faBasketShopping} className='text-green-400 w-[50px] h-[50px]' />
         </p>
---
+++ Client/src/Components/Product/ProductCart.tsx
@@ -0,0 +1,37 @@
+import { faCartPlus, faHeart } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import Image from "next/image"
+
+export interface ProductData{
+    _id: string;
+    title: string;
+    description: string;
+    price: number;
+    discountPercentage?: number;
+    rating?: number;
+    stock?: number;
+    brand?: string;
+    category: string;
+    thumbnail: string;
+    images?: [string];
+}
+
+export const ProductCard = (props: ProductData) => {
+  return (
+    <span key={props._id} className='p-2 duration-300 border-2 border-transparent rounded-md   hover:border-green-400 flex flex-col gap-3 items-center relative'>
+        <Image src={props.thumbnail} style={{width: '250px', height: '250px'}} width={800} height={800} objectFit="contain" alt={props.title} />
+        <span className='text-center'>
+            <p className='text-sm text-right text-green-600'>{props.category}</p>
+            <p className='text-lg'>{props.title}</p>
+            <p className='text-sm text-gray-400'>{props.description}</p>
+            <p className='text-lg'>${props.price}</p>
+        </span>
+        <span className='flex items-center gap-3'>
+            <button className='duration-200 p-3 max-md:p-1 hover:bg-blue-400 text-black hover:text-white font-bold  flex items-center gap-2 rounded-md'> 
+            <FontAwesomeIcon icon={faCartPlus} className='w-[18px] h-[18px]' />
+            Add To Cart</button>
+            <FontAwesomeIcon icon={faHeart} className='w-[18px] h-[18px] text-gray-400 cursor-pointer' />
+        </span>
+    </span>
+  )
+}
