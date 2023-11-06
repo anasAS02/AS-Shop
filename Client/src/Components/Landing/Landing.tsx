6bd340ed58124350e024bd0c1d4866700297da7b
Anas <110332632+anasAS02@users.noreply.github.com>
2023-11-06 12:56:51 +0200

    .

--- Client/src/Components/Landing/Landing.tsx
+++ Client/src/Components/Landing/Landing.tsx
@@ -3,6 +3,9 @@ import Image from 'next/image';
 
 export const Landing = () => {
   return (
-    <Image src={LandingCover} style={{ width: '100%' }} height={0} objectFit="contain" alt='Landing' />
+    <div className='relative'>
+        <h3 className='absolute left-10 top-2/4 text-4xl font-bold w-2/4 leading-10 max-md:text-sm max-md:left-3 -translate-y-2/4'>Welcome to Salla Shop – Your One-Stop Destination for All Your Shopping Needs</h3>
+        <Image src={LandingCover} style={{ width: '100%' }} height={0} objectFit="contain" alt='Landing' />
+    </div>
   )
 }
