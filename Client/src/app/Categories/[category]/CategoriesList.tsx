b3f94860dbd6376f52cca5e39ede64ed4a17fb68
Anas <110332632+anasAS02@users.noreply.github.com>
2023-11-07 15:35:26 +0200

    .

---
+++ Client/src/app/Categories/[category]/CategoriesList.tsx
@@ -0,0 +1,28 @@
+'use client'
+import { links } from '@/Components/Navbar/data';
+import { faAngleDown, faAngleUp } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Link from 'next/link';
+import { useState } from 'react';
+
+export const CategoriesList = (props: {category: string}) => {
+    const [openCategories, setOpenCategories] = useState<boolean>(true);
+    const handleCategories = () => {
+      setOpenCategories(!openCategories);
+    }
+  return (
+    <div>
+        <span className='flex items-center gap-12 mb-2'>Category 
+          <FontAwesomeIcon onClick={handleCategories} icon={openCategories ? faAngleUp : faAngleDown} className='text-green-400 w-[18px] h-[18px] cursor-pointer ml-auto' />
+        </span>
+        <span className={openCategories ? '' : 'hidden'}>
+          {links.map((link) => (
+            <Link href={`/Categories/${link.href}`} key={link.id} className='flex items-center gap-5 mb-2'>
+              <p>{link.title}</p>
+              <span className={`p-2 border-2 rounded-full ml-auto ${link.href === props.category ? 'bg-green-400 border-green-400' : 'bg-white border-gray-400'}`}></span>
+          </Link>
+        ))}
+      </span>
+    </div>
+  )
+}
--- Client/src/app/Categories/[category]/page.tsx
+++ Client/src/app/Categories/[category]/page.tsx
@@ -1,5 +1,6 @@
 import { getCategory } from '@/Utils/Products/getCategory';
 import Image from 'next/image';
+import { CategoriesList } from './CategoriesList';
 
 export async function generateMetedata({params}: any){
   const category = params.category;
@@ -13,9 +14,13 @@ export default async function Category ({params}: any) {
   const category = params.category;
 
   const res = await getCategory(category);
-  const products = res.data.data;
+  const products = res?.data.data;
+
   return (
-    <div>
+    <div className='p-16 flex items-start justify-between gap-10 h-screen'>
+      <aside className='flex flex-col gap-5 '>
+        <CategoriesList category={category} />
+      </aside>
       {products.map((pro: any) => (
         <div key={pro._id}><Image src={pro.images[0]} alt='..' width={200} height={200} /><h2>{pro.title}</h2></div>
       ))}
