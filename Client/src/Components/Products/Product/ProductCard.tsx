8e65ea836950354a7ee53f2fee387454901e2766
Anas <110332632+anasAS02@users.noreply.github.com>
2023-11-08 13:01:50 +0200

    .

--- Client/src/Components/Products/Product/ProductCard.tsx
+++ Client/src/Components/Products/Product/ProductCard.tsx
@@ -7,12 +7,12 @@ export interface ProductData{
     style: boolean;
     _id: string;
     title: string;
-    description: string;
+    description?: string;
     price: number;
     discountPercentage?: number;
     rating?: number;
     stock?: number;
-    brand?: string;
+    brand: string;
     category: string;
     thumbnail: string;
     images?: [string];
@@ -26,17 +26,17 @@ export const ProductCard = (props: ProductData) => {
   return (
     <span key={props._id} className={`w-full p-2 duration-300 border-2 border-transparent rounded-md hover:border-green-400 flex ${props.style && 'flex-col'} gap-6 items-center relative`}>
         <Link href={`/products/product/${props._id}`}>
-            <Image src={props.thumbnail} style={{width: '250px', height: '250px'}} width={800} height={800} objectFit="contain" alt={props.title} />
+            <Image src={props.thumbnail} className='w-[200px] h-[200px] max-md:w-[100px] max-md:h-[100px]' width={800} height={800} objectFit="contain" alt={props.title} />
         </Link>
         <span className={`${props.style && 'text-center'} w-full`}>
             <p className='text-sm text-right text-green-600'>{props.category}</p>
-            <p className='text-lg'>{props.title}</p>
-            <p className='text-sm text-gray-400'>{props.description}</p>
-            <p className={`text-lg ${props.discountPercentage && props.discountPercentage > 0 ? 'line-through text-black texr-sm' : ''}`}>${props.price}</p>
-            {props.discountPercentage && <span className='text-lg text-red-500'>${calc(props.price, props.discountPercentage).toFixed(2)}</span>}
+            <p className='text-lg max-md:text-base'>{props.title}</p>
+            <p className='text-sm text-gray-400'><span className='text-yellow-500'>From: </span>{props.brand}</p>
+            <p className={`text-lg max-md:text-base ${props.discountPercentage && props.discountPercentage > 0 ? 'line-through text-black texr-sm' : ''}`}>${props.price}</p>
+            {props.discountPercentage && <span className='text-lg max-md:text-base text-red-500'>${calc(props.price, props.discountPercentage).toFixed(2)}</span>}
             {!props.style && 
             <span className='flex justify-end items-center gap-3'>
-                <button className='duration-200 p-3 max-md:p-1 hover:bg-blue-400 text-black hover:text-white font-bold  flex items-center gap-2 rounded-md'> 
+                <button className='duration-200 p-3 max-md:p-1 hover:bg-blue-400 text-black hover:text-white font-bold max-md:text-xs flex items-center gap-2 rounded-md'> 
                 <FontAwesomeIcon icon={faCartPlus} className='w-[18px] h-[18px]' />
                 Add To Cart</button>
                 <FontAwesomeIcon icon={faHeart} className='w-[18px] h-[18px] text-gray-400 cursor-pointer' />
@@ -45,7 +45,7 @@ export const ProductCard = (props: ProductData) => {
         </span>
         {props.style &&
         <span className='flex items-center gap-3'>
-        <button className='duration-200 p-3 max-md:p-1 hover:bg-blue-400 text-black hover:text-white font-bold  flex items-center gap-2 rounded-md'> 
+        <button className='duration-200 p-3 max-md:p-1 hover:bg-blue-400 max-md:text-sm text-black hover:text-white font-bold  flex items-center gap-2 rounded-md'> 
         <FontAwesomeIcon icon={faCartPlus} className='w-[18px] h-[18px]' />
         Add To Cart</button>
         <FontAwesomeIcon icon={faHeart} className='w-[18px] h-[18px] text-gray-400 cursor-pointer' />
--- Client/src/app/Categories/[category]/page.tsx
+++ Client/src/app/Categories/[category]/page.tsx
@@ -2,18 +2,18 @@
 
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import { GET_CATEGORY } from '@/Utils/Apis';
+import { GET_CATEGORY, GET_PRODUCTS } from '@/Utils/Apis';
 import { ProductCard, ProductData } from '@/Components/Products/Product/ProductCard';
 import { links } from '@/Components/Navbar/data';
 import Link from 'next/link';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAngleDown, faAngleUp, faBars, faTableCells } from "@fortawesome/free-solid-svg-icons";
+import { faAngleDown, faAngleUp, faBars, faBarsStaggered, faCircleXmark, faTableCells } from "@fortawesome/free-solid-svg-icons";
 
 // export async function generateMetedata({params}: any){
 //   const category = params.category;
 //   return{
 //     title: category,
-//     description: `Salla Shop | ${category}`
+//     description: `SAllProductsa Shop | ${category}`
 //   }
 // }
 
@@ -28,8 +28,9 @@ export default function Category ({params}: any) {
 
   const fetchProducts = async() => {
     try{
-      const res = await axios.get(GET_CATEGORY + category, {
+      const res = await axios.get(category === 'AllProducts' ? GET_PRODUCTS : GET_CATEGORY + category, {
         params: {
+            limit: 10,
             lowestPrice: from,
             highestPrice: to,
             sortByLowestPrice: sortLowest,
@@ -79,13 +80,33 @@ export default function Category ({params}: any) {
     fetchProducts();
   }
 
+  const [openFilter, setOpenFilter] = useState<boolean>(false);
+  const [closeFilter, setCloseFilter] = useState<boolean>(true);
+  
+  const handleOpenFilter = () => {
+    setOpenFilter(true);
+    setCloseFilter(false);
+  }
+
+  const handleCloseFilter = () => {
+    setCloseFilter(true);
+    setOpenFilter(false);
+  }
+
   return (
-    <div className='p-16 flex items-start gap-10 h-full'>
-      <aside className='flex flex-col gap-5 '>
+    <div className={`p-16 max-md:p-4 flex items-start gap-10 ${products?.length > 0 ? 'h-full' : 'h-screen'}`}>
+      <aside className={`${openFilter ? 'flex' : 'hidden'} md:flex flex-col gap-5`}>
         <span className='flex items-center gap-12 mb-2'>Category 
-          <FontAwesomeIcon onClick={handleCategories} icon={openCategories ? faAngleUp : faAngleDown} className='text-green-400 w-[18px] h-[18px] cursor-pointer ml-auto' />
+        <span className='flex items-center gap-3 md:ml-auto'>
+          <FontAwesomeIcon onClick={handleCategories} icon={openCategories ? faAngleUp : faAngleDown} className='text-green-400 w-[18px] h-[18px] cursor-pointer' />
+          <FontAwesomeIcon onClick={handleCloseFilter} icon={faCircleXmark} className='text-red-500 w-[18px] h-[18px] cursor-pointer md:hidden' />
+        </span>
           </span>
           <span className={openCategories ? '' : 'hidden'}>
+            <Link href='/Categories/AllProducts' className={`flex items-center gap-5 mb-2 ${category === 'AllProducts' ? 'text-green-400' : 'text-gray-400'}`}>
+              <p>All Products</p>
+              <span className={`p-2 border-2 rounded-full ml-auto ${category === 'AllProducts' ? 'bg-green-400 border-green-400' : 'bg-white border-gray-400'}`}></span>
+            </Link>
             {links.map((link) => (
               <Link href={`/Categories/${link.href}`} key={link.id} className={`flex items-center gap-5 mb-2 ${category === link.href ? 'text-green-400' : 'text-gray-400'}`}>
                 <p>{link.title}</p>
@@ -96,19 +117,20 @@ export default function Category ({params}: any) {
         <span className='flex flex-col items-center gap-5 mb-2'>
           <p className='mr-auto'>Price</p>
           <span className='flex items-center gap-2'>
-            <input type='number' placeholder='from' onChange={(e) => setFrom(Number(e.target.value))} className='border-none outline-none bg-gray-300 placeholder:text-white p-2 rounded-md w-20' />
-            <input type='number' placeholder='to' onChange={(e) => setTo(Number(e.target.value))} className='border-none outline-none bg-gray-300 placeholder:text-white p-2 rounded-md w-20' />
+            <input type='number' placeholder='from' onChange={(e) => setFrom(Number(e.target.value))} className='border-none outline-none bg-gray-300 placeholder:text-slate-700 p-2 rounded-md w-20' />
+            <input type='number' placeholder='to' onChange={(e) => setTo(Number(e.target.value))} className='border-none outline-none bg-gray-300 placeholder:text-slate-700 p-2 rounded-md w-20' />
           </span>
           <button onClick={(event) => handleFilter(event)} className='p-2 bg-blue-400 duration-300 hover:bg-blue-300 rounded-md text-white'>Filter</button>
         </span>
       </aside>
-      <section className='flex flex-col gap-12 justify-center'>
-          <div className='flex items-center justify-between'>
+      <section className={`${openFilter ? 'hidden' : 'flex'} w-full md:flex flex-col gap-12 justify-between`}>
+          <div className='flex items-center gap-10 justify-between'>
           <span className='flex items-center gap-5'>
+          <FontAwesomeIcon onClick={handleOpenFilter} icon={faBarsStaggered} className={`${openFilter && 'hidden'} md:hidden w-[18px] h-[18px] text-gray-400 cursor-pointer`} />
           <FontAwesomeIcon onClick={() => handleGrid('grid')} icon={faTableCells} className={`w-[18px] h-[18px] ${grid ? 'text-green-400' : 'text-gray-400'} cursor-pointer`} />
           <FontAwesomeIcon onClick={() => handleGrid('')} icon={faBars} className={`w-[18px] h-[18px] ${!grid ? 'text-green-400' : 'text-gray-400'} cursor-pointer`} />
           </span>
-          <span className='flex gap-2 items-center relative'>
+          <span className='flex gap-3 items-center relative max-md:text-sm'>
           Sort By: 
           <select className='border-none outline-none' onChange={(event) => handleSort(event)}>
             <option value='lowest'>Lowest Price</option>
@@ -118,7 +140,7 @@ export default function Category ({params}: any) {
         </div>
         <div className={`gap-5 ${grid ? 'grid items-center grid-cols-4 max-lg:grid-cols-2 max-md:grid-cols-1' : 'flex flex-col items-start justify-start'}`}>
           {products?.map((product: ProductData) => (
-            <ProductCard style={grid ? true : false} key={product._id} _id={product._id} category={product.category} thumbnail={product.thumbnail} title={product.title} price={product.price} description={product.description} discountPercentage={product.discountPercentage} />
+            <ProductCard style={grid ? true : false} key={product._id} _id={product._id} category={product.category} thumbnail={product.thumbnail} title={product.title} price={product.price} brand={product.brand} discountPercentage={product.discountPercentage} />
           ))}
         </div>
       </section>
