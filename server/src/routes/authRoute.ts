dd7945742bc9cca7d47c812369235b0446286971
Anas <110332632+anasAS02@users.noreply.github.com>
2023-11-09 17:29:45 +0200

    .

--- server/src/routes/authRoute.ts
+++ server/src/routes/authRoute.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 const router = express.Router();
 
-import { login, register } from '../controllers/authControllers';
+import { addAdmin, login, register } from '../controllers/authControllers';
 
 router.route('/register')
         .post(register);
 
 router.route('/login')
         .post(login);
+
+router.route('/addAdmin')
+        .post(addAdmin);
 
 export { router as authRoute };
