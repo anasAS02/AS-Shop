006fd2591836bae1b55c086b2c7c1cb34a3623d1
Anas <110332632+anasAS02@users.noreply.github.com>
2023-11-22 16:39:41 +0200

    .

--- Client/src/Components/Products/Latest/LatestProducts.tsx
+++ Client/src/Components/Products/Latest/LatestProducts.tsx
@@ -1,5 +1,6 @@
 import { getProducts } from "@/Utils/Products/getProducts"
 import { ProductCard, ProductData } from "../Product/ProductCard";
+import { calcPrice } from "@/Utils/Products/calcPrice";
 
 export const LatestProducts = async () => {
     const res = await getProducts();
@@ -10,7 +11,7 @@ export const LatestProducts = async () => {
         <h2 className='text-2xl font-bold max-md:text-sm'>Latest Products</h2>
         <div className='grid grid-cols-4 max-lg:grid-cols-2 max-md:grid-cols-1 items-center gap-5'>
             {products?.slice(0, 5).map((product: ProductData) => (
-              <ProductCard style={true} key={product._id} quantity={1} _id={product._id} description={product.description} images={product.images} discountPercentage={product.discountPercentage} title={product.title} thumbnail={product.thumbnail} category={product.category} brand={product.brand} price={product.price} />
+              <ProductCard style={true} key={product._id} quantity={1} _id={product._id} total={calcPrice(product.price, product.discountPercentage || 0)} description={product.description} images={product.images} discountPercentage={product.discountPercentage} title={product.title} thumbnail={product.thumbnail} category={product.category} brand={product.brand} price={product.price} />
             ))}
         </div>
     </div>
