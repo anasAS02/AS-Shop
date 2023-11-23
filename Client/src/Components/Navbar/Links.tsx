fa36baa69b8b90d0ed1dae9fdee16ee5bedc4172
Anas <110332632+anasAS02@users.noreply.github.com>
2023-11-23 20:16:08 +0200

    .

--- Client/src/Components/Navbar/Links.tsx
+++ Client/src/Components/Navbar/Links.tsx
@@ -1,15 +1,32 @@
+// ./src/Components/Navbar/Links.tsx
+
 import { CategoryData, getCategories } from "@/Utils/Products/getCategories";
 import Link from "next/link";
 
-export const Links = async () => {
-  const categories = await getCategories();
+interface LinksProps {
+  categories: CategoryData[];
+}
+
+const Links: React.FC<LinksProps> = ({ categories }) => {
   return (
     <div className='w-full bg-zinc-800 p-2 flex justify-center items-center gap-5 text-sm text-white max-md:flex-wrap'>
-        <Link href={`/Categories/All-Products`} className='duration-200 hover:text-yellow-500'>All Products</Link>
-        {
-        categories?.map((category: CategoryData) => (
+      <Link href={`/Categories/All-Products`} className='duration-200 hover:text-yellow-500'>All Products</Link>
+      {categories.map((category: CategoryData) => (
         <Link key={category._id} href={`/Categories/${category.href}`} className='duration-200 hover:text-yellow-500'>{category.title}</Link>
       ))}
     </div>
-  )
+  );
+};
+
+export async function getStaticProps() {
+  const categories = await getCategories();
+
+  return {
+    props: {
+      categories,
+    },
+    revalidate: 60 * 60,
+  };
 }
+
+export default Links;
