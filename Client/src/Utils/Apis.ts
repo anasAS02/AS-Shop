f91f8fbd3e8799279dd326f31623489179e00633
Anas <110332632+anasAS02@users.noreply.github.com>
2023-11-23 16:12:15 +0200

    .

--- Client/src/Utils/Apis.ts
+++ Client/src/Utils/Apis.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:4000/';
+export const BASE_URL = 'https://as-shop.onrender.com/';
 
 export const REGISTER = `${BASE_URL}auth/register`
 export const LOGIN = `${BASE_URL}auth/login`
