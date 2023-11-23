750fb8c1bc86c9d394bdefb14afdce36f28ee1ff
Anas <110332632+anasAS02@users.noreply.github.com>
2023-11-23 20:36:31 +0200

    .

--- Client/src/app/page.tsx
+++ Client/src/app/page.tsx
@@ -1,6 +1,6 @@
 import { Features } from "@/Components/Features/Features";
 import { Landing } from "@/Components/Landing/Landing";
-import { LatestProducts } from "@/Components/Products/Latest/LatestProducts";
+import LatestProducts from "@/Components/Products/Latest/LatestProducts";
 import { Categories } from "./Categories/Categories";
 import { Cards } from "@/Components/Products/Cards/Cards";
 import { Sponsors } from "@/Components/Sponsors/Sponsors";
