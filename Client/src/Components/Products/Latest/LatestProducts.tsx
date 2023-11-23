fc1cbff3f671304a5f0377706d02045f04fa51c2
Anas <110332632+anasAS02@users.noreply.github.com>
2023-11-23 20:35:58 +0200

    .

--- Client/src/Components/Products/Latest/LatestProducts.tsx
+++ Client/src/Components/Products/Latest/LatestProducts.tsx
@@ -1,19 +1,49 @@
-import { getProducts } from "@/Utils/Products/getProducts"
-import { ProductCard, ProductData } from "../Product/ProductCard";
-import { calcPrice } from "@/Utils/Products/calcPrice";
+'use client'
+import { useEffect, useState } from 'react';
+import { getProducts } from '@/Utils/Products/getProducts';
+import { ProductCard, ProductData } from '../Product/ProductCard';
+import { calcPrice } from '@/Utils/Products/calcPrice';
 
-export const LatestProducts = async () => {
-    const res = await getProducts();
-    const products = res?.data.data;
+const LatestProducts = () => {
+  const [products, setProducts] = useState<ProductData[] | null>(null);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await getProducts();
+        setProducts(response?.data.data);
+      } catch (error) {
+        console.error('Error fetching latest products:', error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
 
   return (
     <div className='flex flex-col gap-5 p-24'>
-        <h2 className='text-2xl font-bold max-md:text-sm'>Latest Products</h2>
-        <div className='grid grid-cols-4 max-lg:grid-cols-2 max-md:grid-cols-1 items-center gap-5'>
-            {products?.slice(0, 5).map((product: ProductData) => (
-              <ProductCard style={true} key={product._id} quantity={1} _id={product._id} total={calcPrice(product.price, product.discountPercentage || 0)} description={product.description} images={product.images} discountPercentage={product.discountPercentage} title={product.title} thumbnail={product.thumbnail} category={product.category} brand={product.brand} price={product.price} />
-            ))}
-        </div>
+      <h2 className='text-2xl font-bold max-md:text-sm'>Latest Products</h2>
+      <div className='grid grid-cols-4 max-lg:grid-cols-2 max-md:grid-cols-1 items-center gap-5'>
+        {products?.slice(0, 5).map((product: ProductData) => (
+          <ProductCard
+            style={true}
+            key={product._id}
+            quantity={1}
+            _id={product._id}
+            total={calcPrice(product.price, product.discountPercentage || 0)}
+            description={product.description}
+            images={product.images}
+            discountPercentage={product.discountPercentage}
+            title={product.title}
+            thumbnail={product.thumbnail}
+            category={product.category}
+            brand={product.brand}
+            price={product.price}
+          />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
+
+export default LatestProducts;
