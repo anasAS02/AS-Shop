ac02bbcceb9b41b9abe7ea8ef8622564795b75da
Anas <110332632+anasAS02@users.noreply.github.com>
2024-08-27 11:44:48 +0300

    update server URL

--- Client/next.config.js
+++ Client/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['i.imgur.com', 'as-shop-ny7g.onrender.com'],
+        domains: ['i.imgur.com', 'as-shop-server.vercel.app'],
     },
 }
 
